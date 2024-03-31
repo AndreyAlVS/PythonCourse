ad1928f5e532285b60877cc14c26b6f7c84c0d40
AndreyAlVS <aavasquezs@gmail.com>
2024-03-31 18:45:07 +0300

    django s2 lag

---
+++ Django/lectures/myapp2/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return f'Username: {self.name}, email: {self.email}, age:{self.age}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'Name: {self.name}, email: {self.email}'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Title is {self.title}'
+
+    def get_summary(self):
+        words = self.content.split()
+        return f'{" ".join(words[:8])}...'

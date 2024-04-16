74fd040a154be1acf83060eb7ec1a8515648b251
AndreyAlVS <aavasquezs@gmail.com>
2024-04-16 12:42:37 +0300

    Django hw_s5

--- Django/homework/myshop/admin.py
+++ Django/homework/myshop/admin.py
@@ -1,8 +1,82 @@
 from django.contrib import admin
+from .models import Order, OrderItem, Client, Product
 
-from django.contrib import admin
-from .models import Order, OrderItem
+
+@admin.action(description="address reset")
+def address_reset(modeladmin, request, queryset):
+    queryset.update(address='')
+
+
+@admin.action(description="quantity_reset")
+def quantity_reset(modeladmin, request, queryset):
+    queryset.update(quantity=0)
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['pk','name', 'email']
+    ordering = ['name']
+    list_per_page = 10
+    search_fields = ['name']
+    actions = [address_reset]
+    fieldsets = [
+        (
+            'Client',
+            {
+                'classes': ['wide'],
+                'fields': ['name'],
+            },
+        ),
+        (
+            'Подробности',
+            {
+                'classes': ['collapse'],
+                'description': 'Client data',
+                'fields': ['email', 'phone_number', 'address'],
+            },
+        ),
+    ]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['prod_name', 'add_date']
+    ordering = ['prod_name']
+    list_per_page = 10
+    search_fields = ['prod_name']
+    actions = [quantity_reset]
+    fieldsets = [
+        (
+            'Product',
+            {
+                'classes': ['wide'],
+                'fields': ['prod_name'],
+            },
+        ),
+        (
+            'Подробности',
+            {
+                'classes': ['collapse'],
+                'description': 'Product description',
+                'fields': ['description'],
+            },
+        ),
+        (
+            'Бухгалтерия',
+            {
+                'fields': ['price', 'quantity'],
+            }
+        ),
+        (
+            'Прочее',
+            {
+                'fields': ['photo'],
+            }
+        ),
+    ]
+
 
 admin.site.register(Order)
 admin.site.register(OrderItem)
-
+# admin.site.register(Client, ClientAdmin)
+# admin.site.register(Product)
